dcca32fc2909f8dc134cf804fb2341867c7cf323
WijoyoBudi <18523191@students.uii.ac.id>
2020-11-30 21:38:38 +0700

    Udah jadi Halaman Beranda + Login

---
+++ app/src/main/java/com/example/cov_idnews/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.cov_idnews;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageSlider imageSlider = findViewById(R.id.slider);
+
+        List<SlideModel> slideModels = new ArrayList<>();
+        slideModels.add(new SlideModel(R.drawable.terpaparcovid, "Image 1"));
+        slideModels.add(new SlideModel(R.drawable.simulasipemilihan, "Image 2"));
+        slideModels.add(new SlideModel(R.drawable.vaksin, "Image 3"));
+        slideModels.add(new SlideModel(R.drawable.coviddeath, "Image 4"));
+        imageSlider.setImageList(slideModels,true);
+    }
+
+    public void admin(View view) {
+        Intent intent = new Intent(MainActivity.this, login_admin.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/cov_idnews/login_admin.java
@@ -0,0 +1,14 @@
+package com.example.cov_idnews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login_admin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_admin);
+    }
+}
