98cbefd82ed5ac6c77986354bcbcedf2e8b9d4c2
Yudifebri <18523074@students.uii.ac.id>
2020-12-03 01:55:37 +0700

    membuat fungsi login admin

---
+++ app/src/main/java/com/example/cov_idnews/halaman_admin.java
@@ -0,0 +1,15 @@
+package com.example.cov_idnews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class halaman_admin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_halaman_admin);
+    }
+}
--- app/src/main/java/com/example/cov_idnews/login_admin.java
+++ app/src/main/java/com/example/cov_idnews/login_admin.java
@@ -2,13 +2,46 @@ package com.example.cov_idnews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.text.Html.fromHtml;
 
 public class login_admin extends AppCompatActivity {
+
+    EditText TxUsername, TxPassword;
+    Button BtnLogin;
+    DBCovid dbCovid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_admin);
+
+        TxUsername = (EditText)findViewById(R.id.txUsername);
+        TxPassword = (EditText)findViewById(R.id.txPassword);
+        BtnLogin = (Button)findViewById(R.id.btnLogin);
+
+        dbCovid = new DBCovid(this);
+
+        BtnLogin.setOnClickListener(v -> {
+            String username = TxUsername.getText().toString().trim();
+            String password = TxPassword.getText().toString().trim();
+
+            if (username.equals("admin") && password.equals("12345")){
+                Toast.makeText(login_admin.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(login_admin.this, halaman_admin.class));
+            }else{
+                Toast.makeText(login_admin.this, "Login Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
+
 }
