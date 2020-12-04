706c77c7bb73396ce00520bbf946a5b1b13940f6
Yudifebri <18523074@students.uii.ac.id>
2020-12-04 07:36:14 +0700

    membuat fungsi logout

--- app/src/main/java/com/example/cov_idnews/halaman_admin.java
+++ app/src/main/java/com/example/cov_idnews/halaman_admin.java
@@ -2,14 +2,32 @@ package com.example.cov_idnews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class halaman_admin extends AppCompatActivity {
+
+    Button Logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halaman_admin);
+
+
+        Logout = (Button) findViewById(R.id.btnExit);
+
+        Logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                Toast.makeText(halaman_admin.this, "Logout Berhasil", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
