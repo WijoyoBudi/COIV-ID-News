1f2faba929c7a609130606970794400b544e088e
zentahapsari <18523163@students.uii.ac.id>
2020-12-06 11:59:32 +0700

    Tambah RS 50%

---
+++ app/src/main/java/com/example/cov_idnews/AddHospital.java
@@ -0,0 +1,23 @@
+package com.example.cov_idnews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+public class AddHospital extends AppCompatActivity {
+
+
+//    private View button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_hospital);
+
+    }
+
+}
