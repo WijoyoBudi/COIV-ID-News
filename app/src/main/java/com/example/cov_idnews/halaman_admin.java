b082d7c4aa9e7e38921a24461a1b16b12761f959
zentahapsari <18523163@students.uii.ac.id>
2020-12-07 16:16:15 +0700

    halaman home admin (2 fitur : input rs rujukan dan berita)

--- app/src/main/java/com/example/cov_idnews/halaman_admin.java
+++ app/src/main/java/com/example/cov_idnews/halaman_admin.java
@@ -6,11 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.example.cov_idnews.rumahsakit.CreateActivity;
 
 public class halaman_admin extends AppCompatActivity {
 
     Button Logout;
+    private ImageView AddHospital;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +30,31 @@ public class halaman_admin extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
                 Toast.makeText(halaman_admin.this, "Logout Berhasil", Toast.LENGTH_SHORT).show();
+
             }
         });
 
+//        AddHospital = (ImageView) findViewById(R.id.AddHospital);
+//        AddHospital.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (v.getId() == R.id.AddHospital) {
+//                    Intent intent = new Intent(halaman_admin.this, CreateActivity.class);
+//                    startActivity(intent);
+//                }
+//            }
+//        });
+//
+    }
+
+
+    public void TambahBerita(View view) {
+        Intent intent = new Intent(halaman_admin.this, AddNews.class);
+        startActivity(intent);
+    }
+
+    public void TambahRumahSakit(View view) {
+        Intent intent = new Intent(halaman_admin.this, CreateActivity.class);
+        startActivity(intent);
     }
 }
